7f87cd60b62a56edadf63d71aab3b0bca9954fe5
carsonedwards0 <62364188+carsonedwards0@users.noreply.github.com>
2020-06-22 14:38:54 -0500

    Add files via upload

---
+++ IBM DS Lab 4.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+L = ["Michael Jackson", 10.1, 1982]
+L
+
+
+# In[2]:
+
+
+print('the same element using negative and positive indexing:\n Positive:', L[0], '\n Negative:' , L[-3] )
+
+
+# In[3]:
+
+
+["Michael Jackson", 10.1, 1982, [1,2], ("A", 1)]
+
+
+# In[4]:
+
+
+L = ["Michael Jackson", 10.1, 1982, "MJ", 1]
+L
+
+
+# In[5]:
+
+
+L[3:5]
+
+
+# In[6]:
+
+
+L.extend(['pop', 10])
+
+
+# In[7]:
+
+
+L
+
+
+# In[8]:
+
+
+L.append(["pop", 10])
+
+
+# In[9]:
+
+
+L
+
+
+# In[10]:
+
+
+a_list = (1,"Hello", [1,2,3], True)
+
+
+# In[11]:
+
+
+a_list
+
+
+# In[14]:
+
+
+a_list[1]
+
+
+# In[16]:
+
+
+a_list[1:4]
+
+
+# In[17]:
+
+
+A = [1,'a']
+B = [2, 1, 'd']
+A + B
+
+
+# In[ ]:
+
+
+
+

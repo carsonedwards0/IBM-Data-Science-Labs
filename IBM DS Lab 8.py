c7ad220a6e8a578cde28a4e7f79981cb058d4482
carsonedwards0 <62364188+carsonedwards0@users.noreply.github.com>
2020-06-22 18:21:54 -0500

    Add files via upload

---
+++ IBM DS Lab 8.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = 5
+a ==6
+
+
+# In[3]:
+
+
+a > 6
+
+
+# In[4]:
+
+
+a > 4
+
+
+# In[7]:
+
+
+a != 3
+
+
+# In[9]:
+
+
+a !=2
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+"ACDC" != "Michael Jackson"
+
+
+# In[11]:
+
+
+"ACDC"
+
+
+# In[12]:
+
+
+"ACDC" == "Michael Jackson"
+
+
+# In[13]:
+
+
+'B' > 'A'
+
+
+# In[14]:
+
+
+'BA' > 'AB'
+
+
+# In[16]:
+
+
+age = 19
+
+if age > 18:
+    print("you can enter")
+    
+print("move on")
+
+
+# In[17]:
+
+
+age = 18
+
+if age > 18:
+    print("you can enter")
+    
+print("move on")
+
+
+# In[18]:
+
+
+age = 18
+
+if age > 18:
+    print("you can enter")
+elif age == 18:
+    print("go see pink floyd")
+else:
+    print("go see meat loaf")
+    
+print("move on")
+
+
+# In[22]:
+
+
+album_year = 1970
+album_year = 1981
+
+if album_year > 1980:
+    print("Album year is greater than 1980")
+    
+print("do something..")
+
+
+# In[25]:
+
+
+album_year = 1979
+
+
+if album_year > 1980:
+    print("Album year is greater than 1980")
+    
+else:
+    print("less than 1980")
+    
+print('do something..')
+
+
+# In[31]:
+
+
+album_year = 1971
+
+if(album_year < 1980) or (album_year > 1989):
+    print("Album was not made in the 1980's")
+
+else:
+    print("The album was made in the 1980's")
+    
+print("")
+print("Do Stuff..")
+
+
+# In[32]:
+
+
+album_year = 1978
+
+if not (album_year == '1984'):
+    print("Album year is not 1984")
+
+
+# In[34]:
+
+
+BackInBlack_Rating = 8.5
+
+if BackInBlack_Rating > 8:
+    print("This album is amazing!!")
+
+
+# In[35]:
+
+
+rating = 8.5
+
+if rating > 8:
+    print("this album is amazing")
+    
+else:
+    print("this album is ok")
+
+
+# In[43]:
+
+
+album_year = 1979
+
+if album_year < 1980 or album_year == 1991 or album_year == 1993:
+    print("this album came out already")
+
+
+# In[ ]:
+
+
+
+

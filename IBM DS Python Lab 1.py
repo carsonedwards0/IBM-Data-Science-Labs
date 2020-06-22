61ddef64ef042a2a966d654e7667843feca2920d
carsonedwards0 <62364188+carsonedwards0@users.noreply.github.com>
2020-06-22 14:09:57 -0500

    Add files via upload

---
+++ IBM DS Python Lab 1.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello, World!")
+
+
+# In[2]:
+
+
+import sys
+print(sys.version)
+
+
+# In[3]:
+
+
+# This line prints a string
+
+
+# In[4]:
+
+
+type(1.0)
+
+
+# In[5]:
+
+
+type(.05)
+
+
+# In[6]:
+
+
+type(.56)
+
+
+# In[7]:
+
+
+sys.float_info
+
+
+# In[8]:
+
+
+int(1.1)
+
+
+# In[9]:
+
+
+int('1')
+
+
+# In[10]:
+
+
+int('1 or 2 people')
+
+
+# In[11]:
+
+
+float('1.2')
+
+
+# In[12]:
+
+
+str(1.2)
+
+
+# In[13]:
+
+
+str(2)
+
+
+# In[14]:
+
+
+True
+
+
+# In[15]:
+
+
+False
+
+
+# In[16]:
+
+
+type(True)
+
+
+# In[17]:
+
+
+type(False)
+
+
+# In[18]:
+
+
+int(True)
+
+
+# In[19]:
+
+
+bool(1)
+
+
+# In[20]:
+
+
+bool(0)
+
+
+# In[21]:
+
+
+float(True)
+
+
+# In[22]:
+
+
+float(False)
+
+
+# In[23]:
+
+
+40 + 60 + 16 + 41
+
+
+# In[24]:
+
+
+50 - 60
+
+
+# In[25]:
+
+
+5*5
+
+
+# In[26]:
+
+
+25 / 25
+
+
+# In[27]:
+
+
+25 \ 6
+
+
+# In[28]:
+
+
+25 / 6
+
+
+# In[29]:
+
+
+25 // 6
+
+
+# In[30]:
+
+
+25 // 5
+
+
+# In[31]:
+
+
+30 + 2 * 60
+
+
+# In[32]:
+
+
+(30 + 20) * 60
+
+
+# In[33]:
+
+
+x = 43 + 60 + 16 + 41
+
+
+# In[34]:
+
+
+x
+
+
+# In[35]:
+
+
+y = x / 60
+
+
+# In[36]:
+
+
+y
+
+
+# In[ ]:
+
+
+
+

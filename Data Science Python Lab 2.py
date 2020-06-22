19472562c4b72a09278b756ea59cb2c07a2e9a23
carsonedwards0 <62364188+carsonedwards0@users.noreply.github.com>
2020-06-22 12:00:24 -0500

    Add files via upload

---
+++ Data Science Python Lab 2.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Name = "Michael Jackson"
+
+
+# In[2]:
+
+
+Name
+
+
+# In[4]:
+
+
+print(Name[0])
+
+
+# In[5]:
+
+
+print(Name[6])
+
+
+# In[6]:
+
+
+print(Name[-1])
+
+
+# In[7]:
+
+
+len("Michael Jackson")
+
+
+# In[8]:
+
+
+Name[0:4]
+
+
+# In[9]:
+
+
+Name[8:12]
+
+
+# In[10]:
+
+
+Name[::2]
+
+
+# In[11]:
+
+
+Name[0:5:2]
+
+
+# In[12]:
+
+
+Statement = Name + "is the best"
+
+
+# In[13]:
+
+
+Statement
+
+
+# In[14]:
+
+
+3 * "Michael Jackson"
+Name = Name + " is the best"
+Name
+
+
+# In[15]:
+
+
+print(" Michael Jackson \n is the best" )
+
+
+# In[16]:
+
+
+print(" Michael Jackson \\ is the best")
+
+
+# In[18]:
+
+
+print(r" Michael Jackson \ is the best" )
+
+
+# In[19]:
+
+
+A = "Thriller is the sixt studio album" 
+print("before upper:", A)
+B = A.upper()
+print("After upper:", B)
+
+
+# In[20]:
+
+
+A = "Michael Jackson is the best"
+B = A.replace( 'Michael', 'Janet')
+B
+
+
+# In[21]:
+
+
+Name = "Michael Jackson"
+Name.find('el')
+
+
+# In[22]:
+
+
+Name.find('Jack')
+
+
+# In[23]:
+
+
+Name.find('Jasdfasdasdf')
+
+
+# In[24]:
+
+
+C = A + B
+
+
+# In[25]:
+
+
+A = "1"
+B = "2"
+C = A + B
+
+
+# In[26]:
+
+
+C
+
+
+# In[27]:
+
+
+D = "ABCDEFG"
+print(D[0:2])
+
+
+# In[28]:
+
+
+E = 'clockrkr1e1c1t'
+print(E[::2])
+
+
+# In[30]:
+
+
+print("The bird went to the \\ mall")
+
+
+# In[31]:
+
+
+F = "Your are wrong"
+F.upper
+
+
+# In[33]:
+
+
+F.upper()
+
+
+# In[ ]:
+
+
+G = "Mary had a little lamb little lam"
+
